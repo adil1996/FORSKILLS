6ab21bf1a7e7a61a22f842f52a89160d95b1b64c
Saiyyad Mohammad Adil <SaiyyadMohammad.Adil@mrcooper.com>
2020-11-09 06:41:02 -0600

    Updates

---
+++ precache-manifest.f10d33635f1c32fa9c8b0041bb4f7afe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f57ef268548f8e134f3f046fcdd554",
+    "url": "/FORSKILLS/index.html"
+  },
+  {
+    "revision": "c2e6350f8702d0c26aca",
+    "url": "/FORSKILLS/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ef836dcf7fd6a13d6ac8",
+    "url": "/FORSKILLS/static/css/main.6999cb04.chunk.css"
+  },
+  {
+    "revision": "c2e6350f8702d0c26aca",
+    "url": "/FORSKILLS/static/js/2.48daa646.chunk.js"
+  },
+  {
+    "revision": "ef836dcf7fd6a13d6ac8",
+    "url": "/FORSKILLS/static/js/main.990df938.chunk.js"
+  },
+  {
+    "revision": "dd3a0f6e50da382f0543",
+    "url": "/FORSKILLS/static/js/runtime-main.41a90188.js"
+  },
+  {
+    "revision": "5e8d9f61643078fa490e266e00540c87",
+    "url": "/FORSKILLS/static/media/crea.5e8d9f61.jpg"
+  },
+  {
+    "revision": "d2e9e903e61454aa4b44354386ff408b",
+    "url": "/FORSKILLS/static/media/ed.d2e9e903.jpg"
+  }
+]);
